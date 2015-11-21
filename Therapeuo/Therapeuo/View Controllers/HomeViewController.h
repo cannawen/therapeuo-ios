c62b5e625f5ce2471dbc4353085b45e5fa3fab1b
Canna Wen <cannawen@gmail.com>
2015-11-21 10:08:00 -0500

    Adding initial view controllers

---
+++ Therapeuo/Therapeuo/View Controllers/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  Therapeuo
+//
+//  Created by Canna Wen on 2015-11-21.
+//  Copyright © 2015 Dumpling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
---
+++ Therapeuo/Therapeuo/View Controllers/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Therapeuo
+//
+//  Created by Canna Wen on 2015-11-21.
+//  Copyright © 2015 Dumpling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
