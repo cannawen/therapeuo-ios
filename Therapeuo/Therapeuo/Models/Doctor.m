3152857f3030ee28425effa68889b94f3622e41f
Brian Shim <Brian@Brian-Air.local>
2015-11-21 08:57:15 -0500

    Added initial Model files

---
+++ Therapeuo/Therapeuo/Models/Doctor.m
@@ -0,0 +1,13 @@
+//
+//  Doctor.m
+//  Therapeuo
+//
+//  Created by Brian Shim on 2015-11-21.
+//  Copyright © 2015 Dumpling. All rights reserved.
+//
+
+#import "Doctor.h"
+
+@implementation Doctor
+
+@end
