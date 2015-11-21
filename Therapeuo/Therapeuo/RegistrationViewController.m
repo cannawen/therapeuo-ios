1888223734d0e72369bad9cb76281815b4b17804
Canna Wen <cannawen@gmail.com>
2015-11-21 10:17:28 -0500

    Added outlet for registration view

--- Therapeuo/Therapeuo/RegistrationViewController.m
+++ Therapeuo/Therapeuo/RegistrationViewController.m
@@ -10,28 +10,16 @@
 
 @interface RegistrationViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+
 @end
 
 @implementation RegistrationViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+- (IBAction)registerButtonTapped:(id)sender {
+    //TODO: Steven to make network call
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
