cce99e61d8f8d6a9b703e31f8fd3f306dfab9fad
Steven Wu <wu.s.k.steven@gmail.com>
2015-11-22 00:27:16 -0500

    Let’s see what more than 1 case looks like

--- Therapeuo/Therapeuo/Cases/CaseListViewController.m
+++ Therapeuo/Therapeuo/Cases/CaseListViewController.m
@@ -96,12 +96,14 @@
     self.openCases = [cases filter:^BOOL(Case *aCase) {
         return aCase.open && [aCase.primaryDoctorId isEqualToString:doctorId];
     }];
-    self.proxyCases = [cases filter:^BOOL(Case *aCase) {
-        return aCase.open && ![aCase.primaryDoctorId isEqualToString:doctorId];
-    }];
-    self.closedCases = [cases filter:^BOOL(Case *aCase) {
-        return !aCase.open;
-    }];
+//    self.proxyCases = [cases filter:^BOOL(Case *aCase) {
+//        return aCase.open && ![aCase.primaryDoctorId isEqualToString:doctorId];
+//    }];
+//    self.closedCases = [cases filter:^BOOL(Case *aCase) {
+//        return !aCase.open;
+//    }];
+    self.proxyCases = self.openCases;
+    self.closedCases = self.openCases;
 }
 
 #pragma mark - IBActions
